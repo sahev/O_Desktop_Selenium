fd304e1a111b42b8f7fe5deb4be501828391bd5f
Samuel Evangelista <samuel.evangelista@orbium.com.br>
2020-06-28 21:35:42 -0300

    Alter path of html archives

--- Orbium_5_10_1/Report Test/generate report.bat
+++ Orbium_5_10_1/Report Test/generate report.bat
@@ -2,4 +2,4 @@ del ".\xml\*.xml"
 del ".\*.html"
 
 .\References\nunit3-console.exe "C:\working\Orbium_5_10_1\Orbium_5_10_1\Orbium_5_10_1\bin\Debug\Orbium_Desktop_5_10_1.dll" --result="CT01CustomerEdit.xml;format=nunit3" --work=".\xml
-.\References\ReportUnit.exe "C:\working\Orbium_5_10_1\Orbium_5_10_1\Orbium_5_10_1\Report Test\xml" "C:\working\Orbium_5_10_1\Orbium_5_10_1\Orbium_5_10_1\Report Test"
+.\References\ReportUnit.exe ".\xml" ".\"
